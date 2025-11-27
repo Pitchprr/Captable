8200ae33968edb242178f231800d8d5cb8fd8036
Paul-Henri <paulhenri@angelsquare.co>
2025-11-27 11:49:57 +0100

    autolaunch fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Captable/', // GitHub Pages base path (repository name)
+  server: {
+    open: true,
+    port: 5173
+  }
 })
